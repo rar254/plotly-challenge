63aadac2e77392d9eb167217bfbdc5c166f8ffed
Rafael <stephany@Rafaels-Air.fios-router.home>
2021-03-01 23:05:02 -0500

    function to select metadata

--- static/js/app.js
+++ static/js/app.js
@@ -4,14 +4,30 @@ Feel free to disregard and create your own code */
 
 // Define a function that will create metadata for given sample
 function buildMetadata(sample) {
-
     // Read the json data
-
+    d3.json("../../samples.json").then(function(data){
+        //SELECT data and test
+        var metaData = data.metadata;
+        console.log(sample);
+        //create a filter to select the desired data(sample)
+        var filter = metaData.filter(otu =>otu.id == sample);
+        //check the filtered data
+        console.log(filter);
         // Parse and filter the data to get the sample's metadata
-
+        var data = filter[0];
+        console.log(data);
+        var display = d3.select("#sample-metadata")
+        //clear and input our new data into our html
+        display.html("");
         // Specify the location of the metadata and update it
-
-}
+        //break data into key,value to append
+        Object.entries(data).forEach(([key, value])=> {
+            console.log(key,value);
+            //add our data using h4 anchor tag/text
+            display.append("h4").text(`${key}: ${value}`);
+        });
+    });
+};
 
 // Define a function that will create charts for given sample
 function buildCharts(sample) {
